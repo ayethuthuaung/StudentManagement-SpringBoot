4fca7ae47cc6359d7de80db9b1b6bd6fd7acbb29
Vinnnm <148937860+Vinnnm@users.noreply.github.com>
2024-05-26 23:32:04 +0630

    Update SecurityConfig

--- src/main/java/com/student/config/SecurityConfig.java
+++ src/main/java/com/student/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filter(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
 //                .cors(Customizer.withDefaults())
 //                .httpBasic(AbstractHttpConfigurer::disable)
